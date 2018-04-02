d789bb63f47341dfb767bd8fbbb10f5798f0d7d7
Russ Wilkie <russwilkie@gmail.com>
2018-04-02 10:24:38 -0500

    removed commented out code blocks

--- js/freelancer.js
+++ js/freelancer.js
@@ -75,13 +75,6 @@
 
   $(window).resize(toggleLogo);
 
-  // Modal popup$(function () {
-  /* $('.portfolio-item').magnificPopup({
-     type: 'inline',
-     preloader: false,
-     focus: '#username',
-     modal: true
-   });*/
   $(document).on('click', '.portfolio-modal-dismiss', function (e) {
     e.preventDefault();
     $.magnificPopup.close();
